d5bdca7d175e9a25659513d196802edd4c5071a1
kshitizagarwal-dev <76765982+kshitizagarwal-dev@users.noreply.github.com>
2020-12-30 11:50:35 +0530

    Add files via upload

---
+++ Clouds.kt
@@ -0,0 +1,7 @@
+package com.example.weather2.models
+
+import java.io.Serializable
+
+data class Clouds (
+        val all : Int
+): Serializable
---
+++ Constants.kt
@@ -0,0 +1,39 @@
+package com.example.weather2
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+object Constants{
+
+    const val APP_ID : String = "50d2f51007156a0270f9086f5d57aa87"
+    const val BASE_URL : String = "http://api.openweathermap.org/data/"
+    const val METRIC_UNIT : String = "metric"
+
+    const val PREFERENCE_NAME = "WeatherAppPreference"
+    const val WEATHER_RESPONSE_DATA = "weather_response_data"
+
+    fun isNetworkAvailable(context: Context) : Boolean{
+        val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE)
+                        as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network)?: return false
+
+            return when{
+
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        }
+
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo !=null && networkInfo.isConnectedOrConnecting
+    }
+}
